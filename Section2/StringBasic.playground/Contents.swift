6c34cc348362808fe78879d6c47be6ae5c530aae
n913239 <n913239@gmail.com>
2017-09-12 15:48:46 +0800

    add S2 Lab 15 ~ 16

---
+++ Section2/StringBasic.playground/Contents.swift
@@ -0,0 +1,17 @@
+var firstNumber:Int = 18
+let secondNumber:Double = 12.34
+
+// Boolean
+var isTheLightsOn:Bool = true
+isTheLightsOn = false
+
+let threeImportantWords = "I Love you"
+
+let language:String = "Swift"
+
+let isCool:Bool = true
+
+var version:Double = 3.0
+
+
+
