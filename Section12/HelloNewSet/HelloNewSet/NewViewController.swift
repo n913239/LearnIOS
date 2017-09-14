7280272be8651ae3867a7acab68edef92c4ad815
n913239 <n913239@gmail.com>
2017-09-15 02:26:23 +0800

    add S12 Lab 102, 103 HelloNewSet

---
+++ Section12/HelloNewSet/HelloNewSet/NewViewController.swift
@@ -0,0 +1,17 @@
+//
+//  NewViewController.swift
+//  HelloNewSet
+//
+//  Created by mikewang on 2017/9/15.
+//  Copyright © 2017年 mike. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class NewViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.yellow
+    }
+}
---
+++ Section12/HelloNewSet/HelloNewSet/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  HelloNewSet
+//
+//  Created by mikewang on 2017/9/15.
+//  Copyright © 2017年 mike. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.red
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
+class AnotherViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.blue
+    }
+}
+
