a2c43f7d26f11e712ee458733651a6b45332a1d5
n913239 <n913239@gmail.com>
2017-09-19 18:26:09 +0800

    add S23 Lab 221 ~ 223 QuickDial

---
+++ Section23/QuickDial/QuickDial/BigLabelButton.swift
@@ -0,0 +1,21 @@
+//
+//  BigLabelButton.swift
+//  QuickDial
+//
+//  Created by mikewang on 2017/9/19.
+//  Copyright © 2017年 mike. All rights reserved.
+//
+
+import UIKit
+
+class BigLabelButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
